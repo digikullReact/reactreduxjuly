dc7f87fcd821e0524ee0bbf4acee9787d0dbda3b
ajayjhaDev <ajaykumarjha05072000@gmail.com>
2022-11-28 19:03:42 +0530

    added other

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import {increment} from "./slice";
-import {useDispatch,useSelector} from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+import { increment, decrement, incrementByAmount } from "./slice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const state=useSelector(state=>state.counter);
-  const dispatch =useDispatch();
+  const state = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   console.log(state);
 
-  const  handleClick=()=>{
+  const handleClick = () => {
     dispatch(increment());
+  };
 
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
 
-  }
+  const handleincrementBy10 = () => {
+    dispatch(incrementByAmount(10));
+  };
   return (
     <div className="App">
-
       <h1>{state.value}</h1>
 
+      <button onClick={handleClick}>Increment</button>
 
-      <button onClick={handleClick}>
+      <button onClick={handleDecrement}>Decrement</button>
 
-       Increment
-
-      </button>
-     
+      <button onClick={handleincrementBy10}>incrementBy10</button>
     </div>
   );
 }
